962d65bfc5aa167849c552848e6ff475d602d38f
samanthabertous <sam@samanthabertous.com>
2016-05-03 12:29:39 -0400

    Added dynamic bootstrap image slider and pop up contact form using bootstrap and ninja forms

--- wp-content/themes/bootstrap-to-wp/single.php
+++ wp-content/themes/bootstrap-to-wp/single.php
@@ -9,6 +9,15 @@
          
           <div class="page-header">
             <h1><?php the_title(); ?> </h1>
+            <!-- Display Featured image -->
+
+              <?php
+                $thumbnail_id = get_post_thumbnail_id(); 
+                $thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
+                $thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true);                
+              ?>
+              <p><img src="<?php echo $thumbnail_url[0]; ?>" alt="<?php echo $thumbnail_meta; ?>"></p>
+
             <p><em>
               By <?php the_author();?> 
               on <?php echo the_time('l, F jS, Y'); ?>
